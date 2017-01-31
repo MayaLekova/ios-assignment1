7badaa4ac11a19a2f0c51a9af6e2a14863cfd015
Maya Lekova <apokalyptra@gmail.com>
2017-01-31 10:20:45 +0100

    Add search bar as header to the movies table

--- Assignment1/Assignment1/ViewController.swift
+++ Assignment1/Assignment1/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
             self.tableView.reloadData()
         }
     }
+    
+    var currentPage = 0
+    
+    let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let nib = UINib(nibName: "MoviePreviewTableViewCell", bundle: nil)
         self.tableView.register(nib, forCellReuseIdentifier: "MoviePreviewTableViewCell")
+        
+        // SearchResultsUpdater allows class to be informed of text changes
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.definesPresentationContext = true
+        
+        // Attach the newly created searchBar to the table's header
+        tableView.tableHeaderView = searchController.searchBar
+        
+        // TODO: Localize titles
+        searchController.searchBar.scopeButtonTitles = ["Movie", "Episode", "Series", "All"]
+        searchController.searchBar.delegate = self
         
         // Register to receive notification data
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.notifyObservers), name:  NSNotification.Name(rawValue: "gotMovieData"), object: nil)
@@ -53,6 +69,8 @@ class ViewController: UIViewController {
     
 }
 
+// MARK: TableViewDataSource
+
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (episodes?.count) ?? 0
@@ -66,3 +84,28 @@ extension ViewController: UITableViewDataSource {
         return cell
     }
 }
+
+// MARK: Search Scheduler
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for: UISearchController) {
+        // TODO fire scheduleSearch
+    }
+}
+
+// MARK: Search Delegate
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // TODO
+    }
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        // TODO
+        self.currentPage = 1
+        return true
+    }
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        // TODO
+    }
+}
+
