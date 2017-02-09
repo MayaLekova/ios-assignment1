e8c2d7b5babbde8fbd61e986b0df84ed03f1a1b8
Maya Lekova <apokalyptra@gmail.com>
2017-02-09 12:07:23 +0100

    Embed main ViewController in a tab bar and add Favourites tab

---
+++ Assignment1/Assignment1/FavouritesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FavouritesViewController.swift
+//  Assignment1
+//
+//  Created by Maya Lekova on 2/9/17.
+//  Copyright © 2017 Maya Lekova. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
